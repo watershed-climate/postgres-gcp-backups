dbfa95417abedfdde945ae1118bcad04833ef81f
Will Moss <will.moss@watershedclimate.com>
2023-10-01 17:52:04 -0700

    Fix the filename for uploading to GCS (#3)
    GCS doesn't seem to like to rename the filename during a cp, so we name the local file correctly and then upload it.

--- backup.sh
+++ backup.sh
@@ -2,33 +2,19 @@
 
 set -o errexit -o nounset -o pipefail
 
-auth_gcloud() {
-    gcloud config set project watershed-security-and-privacy
-    gcloud auth login --cred-file=$GCS_AUTH_KEY_FILE
-}
+filename="$(date +%Y-%m-%dT%H-%MZ.sql.gz)"
 
-pg_dump_database() {
-    # Set statement_timeout to 0 to prevent the dump from timing out
-    export PGOPTIONS="-c statement_timeout=0"
+echo "Backing up Postgres database into GCS file $filename..."
 
-    pg_dump --no-owner --no-privileges --clean --if-exists --quote-all-identifiers -n public "$DATABASE_URL"
-}
+gcloud auth activate-service-account --key-file=$GCS_AUTH_KEY_FILE \
+  --project=watershed-security-and-privacy
 
-upload_to_bucket() {
-    # See https://cloud.google.com/sdk/gcloud/reference/storage/cp
-    gcloud storage cp backup.sql.gz "gs://$GCS_BUCKET_NAME/$(date +%Y-%m-%dT%H-%MZ.sql.gz)"
-}
+# Set statement_timeout to 0 to prevent the dump from timing out
+export PGOPTIONS="-c statement_timeout=0"
 
-main() {
-    auth_gcloud
+pg_dump --no-owner --no-privileges --clean --if-exists --quote-all-identifiers \
+  -n public "$DATABASE_URL" \
+  | gzip \
+  | gcloud storage cp - "gs://$GCS_BUCKET_NAME/$filename"
 
-    echo "Taking a backup of Postgres database..."
-    pg_dump_database | gzip > backup.sql.gz
-
-    echo "Uploading it to GCS..."
-    upload_to_bucket
-
-    echo "Done."
-}
-
-main
+echo "Done."
